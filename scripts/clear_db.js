cddcd60ab46595e2a4b47561a047e7984699737d
Mukesh <shyamkrishna@Mukesh-MacBook-Air.local>
2026-08-14 11:03:38 +0530

    Update clear_db.js options

--- scripts/clear_db.js
+++ scripts/clear_db.js
@@ -5,7 +5,10 @@ const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://mdharixtechsolutions
 
 async function clearMongo() {
   console.log("Connecting to MongoDB Atlas at:", MONGO_URI);
-  await mongoose.connect(MONGO_URI, { serverSelectionTimeoutMS: 10000 });
+  await mongoose.connect(MONGO_URI, {
+    family: 4,
+    serverSelectionTimeoutMS: 15000
+  });
   console.log("Connected!");
   const collections = await mongoose.connection.db.collections();
   for (let collection of collections) {
